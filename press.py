ae1651e7d2a6daffa621b3066d2a1d660027479a
pandahop <mikikik2520@gmail.com>
2021-08-01 12:20:23 +0800

    added chinese version

--- press.py
+++ press.py
@@ -4,8 +4,10 @@ from time import sleep
 DELAY=5
 
 instruction1='''make sure first page of magna is opened and enter the number of pages in this chapter:'''
+instruction1='''使用前请将浏览器调到想看章节的第一页，输入想看的页数：'''
 pages=int(input(instruction1))
 instruction2='''how many page downs to press on a single page:'''
+instruction2='''输入每页按多少次翻页键：'''
 downs_per_page=int(input(instruction2))+1
 hotkey('alt', 'tab') # switch to browser
 for i in range(pages):
